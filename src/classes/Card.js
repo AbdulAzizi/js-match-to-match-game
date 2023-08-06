7efd26963d7e739013901b0568482f07537a0c0e
AbdulAziz Nurov <nurovaziz@gmail.com>
2023-08-06 15:55:47 +0500

    Initialize Card class

---
+++ src/classes/Card.js
@@ -0,0 +1,14 @@
+class Card {
+	image;
+	html = '<div class="card"></div>';
+	isFlipped = false;
+
+	constructor({ image }) {
+		this.image = image;
+	}
+
+	flip() {
+		// flit the card
+	}
+}
+export default Card;
